1f7e0006c7aa68abc1ad44012ced6d6fa534fccf
Cleo Menezes <produtor.junior.menezes@gmail.com>
2020-07-13 00:40:47 -0400

    Version 0.0.4

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ layout = [
     [sg.Text(f'QRcodeX', size=(25, 1), justification='center', font=('Helvetica', 14))],
     [sg.Text('Link', font=('Helvetica', 10)),
      sg.InputText(key='input', size=(35, 1), justification='r', font=('Lucida faz', 10))],
-    [sg.Button('Gerar', font=('Helvetica', 10), size=(17, 3), button_color=('black', 'light blue')),
-     sg.Button('Fechar', font=('Britannic', 10), size=(17, 3), button_color=('black', 'light blue'))],
-    [sg.Image('Images\qrcreated.PNG', key=2, size=(290, 300))]
+    [sg.Button('Create', font=('Helvetica', 10), size=(17, 3), button_color=('black', 'light blue')),
+     sg.Button('Close', font=('Britannic', 10), size=(17, 3), button_color=('black', 'light blue'))],
+    [sg.Image(r'Images\qrcreated.PNG', key=2, size=(290, 300))]
 
 ]
-window = sg.Window('QRcodeX - Gerador de QRCode', layout, alpha_channel=0.9)
+window = sg.Window('QRcodeX - Gerador de QRCode', layout, alpha_channel=0.9, grab_anywhere=True, no_titlebar=True, keep_on_top=True, auto_size_buttons=False)
 
 while True:
     event, values = window.Read()
 
-    if event == 'Fechar':
+    if event == 'Close':
         window.close()
         sg.popup_auto_close('''Created by Cleo Menezes
-                Github: github.com/CleoMenezes''')
+        Github: github.com/CleoMenezes''', no_titlebar=True)
         break
-    elif event == 'Gerar':
+    elif event == 'Create':
         values_on_input = (values['input'])
         img = qrcode.make(values_on_input, border=0, version=1)
-        img.save(f'Images\qrcreated.PNG')
-    window[2].Update(f'Images\qrcreated.PNG')
+        img.save(r'Images\qrcreated.PNG')
+    window[2].Update(r'Images\qrcreated.PNG')
 
 window.close()
